b42d051b712dd199d45e23b5f21468a29a6c8d23
Will7136 <89648548+Will7136@users.noreply.github.com>
2022-02-16 15:19:34 +0000

    Update DrawGrid.cs

--- DrawGrid.cs
+++ DrawGrid.cs
@@ -25,34 +25,35 @@ public class DrawGrid : MonoBehaviour {
 		FloorTiles = new GameObject ("FloorTiles").transform;	//Creates a floorTiles game object that acts as the parent to the rest of the grid tiles
         gridObject = GameObject.FindObjectOfType<GridArray>();	//gets the grid array
 		gridObject.generate();									// generates a grid array
-		gridArray = gridObject.getArray();
+		gridArray = gridObject.getArray();	//assigns the actual array made by grid object to an array
 
-		for (int y = 0;y<gridObject.yDimension;y++){
-			for(int x = 0;x<gridObject.xDimension;x++){
+		for (int y = 0;y<gridObject.yDimension;y++){		//loops through every entry in the array from
+			for(int x = 0;x<gridObject.xDimension;x++){		//top to bottom, left to right
 				GameObject Instance = Instantiate(FloorTile, new Vector3(x,-y,0f), Quaternion.identity) as GameObject;
-				Instance.transform.SetParent(FloorTiles);
-				if (gridArray[y,x] == 1){
+				Instance.transform.SetParent(FloorTiles);	//Creates an instance of a floor tile to create the grid and childs it to an empty gameobject
+				if (gridArray[y,x] == 1){	// find if the player is on this tile
 					GameObject PInstance = Instantiate(PlayerTile, new Vector3(x,-y,0f), Quaternion.identity) as GameObject;
-					PInstance.transform.SetParent(FloorTiles);
+					PInstance.transform.SetParent(FloorTiles);	//creates an instance of a player object on this tile
 				}
-				else if (gridArray[y,x] == 2){
+				else if (gridArray[y,x] == 2){	// find if an enemy is on this tile
 					GameObject EInstance = Instantiate(EnemyTile, new Vector3(x,-y,0f), Quaternion.identity) as GameObject;
-					EInstance.transform.SetParent(FloorTiles);
+					EInstance.transform.SetParent(FloorTiles);	//creates an instance of an enemy object on this tile
 				}
-				else if (gridArray[y,x] == 3){
+				else if (gridArray[y,x] == 3){	// find if there is an item on this tile
 					GameObject IInstance = Instantiate(ItemTile, new Vector3(x,-y,0f), Quaternion.identity) as GameObject;
-					IInstance.transform.SetParent(FloorTiles);
+					IInstance.transform.SetParent(FloorTiles);	//creates an instance of an item object on this tile
 				}
-				else if (gridArray[y,x] == 4){
+				else if (gridArray[y,x] == 4){	// find if there is a barrier on this tile
 					GameObject BInstance = Instantiate(BarrierTile, new Vector3(x,-y,0f), Quaternion.identity) as GameObject;
-					BInstance.transform.SetParent(FloorTiles);
+					BInstance.transform.SetParent(FloorTiles);	//creates an instance of a barrier object on this tile
 				}
-
-			}
-		}
-
+				//Tiles are placed at location x, -y, 0f as the x position on the grid will always be the same as the position in the table 
+			}	//eg when x is 4, the actual coordinate in game will be (x, y, 0f)
+		}		// -y is used since, as y increases in the loop, you are actually moving down the table. 
+				// without using -y the grid would be built upside down (ie the coordiante (2,1) in a 5x5 table would be (2,5) in game)
 		FloorTiles.transform.position = new Vector3(-(((gridObject.xDimension) -1.0f)/2.0f), ((gridObject.yDimension -1.0f)/2.0f),0);
-
+				// This then moves the position of the parent containing all tiles to center the whole grid. 
+				//The position itself is dependant on the width and height of the grid.
     }
 	
 }
