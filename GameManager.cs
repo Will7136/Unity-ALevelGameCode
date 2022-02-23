33f818fae57180171d00a1cd68d76bc8a1fcd11a
Will7136 <89648548+Will7136@users.noreply.github.com>
2022-02-23 16:19:23 +0000

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	private GridArray gridObject;
+	private NodeArrayCreator nodeArrayCreator;
+	public Nodeclass[,] nodeArray;
+	private Enemy enemy;
+	
+
+	void Update () {
+		if (Input.GetKeyDown("space")){
+			nodeArrayCreator.beginNodeArr();
+			nodeArray = nodeArrayCreator.getNodeArray();
+			testPathfind();
+			return;
+		}
+	}
+
+	void Start(){
+		gridObject = GameObject.FindObjectOfType<GridArray>();
+		nodeArrayCreator = GameObject.FindObjectOfType<NodeArrayCreator>();
+		enemy = GameObject.FindObjectOfType<Enemy>();
+	}
+
+	public Nodeclass getNode(int x, int y){
+
+		Nodeclass node = nodeArray[y,x];
+		return(node);
+	}
+
+	public Nodeclass getEnemyNode(){
+		for (int y = 0;y<gridObject.yDimension;y++){		
+			for(int x = 0;x<gridObject.xDimension;x++){
+				if (nodeArray[y,x].contents == 2){
+					Nodeclass eNode = nodeArray[y,x];
+					return(eNode);
+				}
+			}
+		}
+		return(null);
+	}
+	public Nodeclass getPlayerNode(){
+		for (int y = 0;y<gridObject.yDimension;y++){		
+			for(int x = 0;x<gridObject.xDimension;x++){
+				if (nodeArray[y,x].contents == 1){
+					Nodeclass pNode = nodeArray[y,x];
+					return(pNode);
+				}
+			}
+		}
+		return(null);
+	}
+
+	public int getXDimension(){
+		return(gridObject.xDimension);
+	}
+	public int getYDimension(){
+		return(gridObject.yDimension);
+	}
+
+	private void testPathfind(){
+		List<Nodeclass> path = new List<Nodeclass>();
+		path = enemy.pathfind();
+		string pathString = "";
+		for (int x = 0; x < path.Count; x++){
+			string nextNode = "(" + (path[x].x).ToString() + ", " + (path[x].y).ToString() + ")";
+			pathString = pathString + nextNode + "   ";
+		}
+
+		Debug.Log(pathString);
+	}
+}
